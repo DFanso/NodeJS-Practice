b20c40a394ba5de98d043a36ee729a68e7a12716
Leo Gavin <leogavin123@outlook.com>
2023-03-11 14:59:02 +0530

    //4) Pagination

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -32,7 +32,16 @@ exports.getAllTours = async (req, res) => {
     }
 
     //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 1;
+    const skip = (page - 1) * limit;
 
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page is not existing');
+    }
     //execute query
     const tours = await query;
 
