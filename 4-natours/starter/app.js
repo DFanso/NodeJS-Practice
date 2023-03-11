da9eb8d5d1f81fa67a4a76b4ab61f5982d0f0254
Leo Gavin <leogavin123@outlook.com>
2023-03-11 22:37:00 +0530

    aggregate pipeline

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,37 +1,35 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
-
 //(1) Middleware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello From the middleware 👋🏻');
-    next();
+  // eslint-disable-next-line no-console
+  console.log('Hello From the middleware 👋🏻');
+  next();
 });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-
-
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
-
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
