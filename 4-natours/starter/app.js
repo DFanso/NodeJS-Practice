a0c9492533bbc5447d6b70bb601ca2b08c050ff5
Leo Gavin <leogavin123@outlook.com>
2023-04-06 23:50:20 +0530

    error handling in express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,7 +5,8 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -19,17 +20,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log('Hello From the middleware 👋🏻');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+//routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
